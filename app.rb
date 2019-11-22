4ba1c5ca6522d02823423efcdd4a4f1c36792f62
alexiscait142 <chilinski.a@gmail.com>
2019-11-22 10:12:45 -0700

    all tests passing

--- app.rb
+++ app.rb
@@ -9,8 +9,29 @@ end
 
 companies = [
     Company.new('Alpha', 30),
-    Company.new('Beta, 300),
-    Company.new('Delta, 3000)
+    Company.new('Beta', 300),
+    Company.new('Delta', 3000)
 ]
 
 ### Your code below this
+
+# companies.map do |instance| puts "#{instance.name} - #{instance.size}"
+# end
+
+# puts companies.reduce(0){|memo, instance| memo + instance.size}
+
+# companies.select do |instance|
+#     if instance.size > 100
+#     puts instance.name
+#     end
+# end
+
+# puts companies.find{|instance| instance.name = "Beta"}
+
+# puts companies.max_by{|instance| instance.size}
+
+# puts companies.sort{|instance| instance.size}
+
+
+
+
